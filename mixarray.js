fdfcba7ed5d8b363a4da751248ef9730acc8d873
Foyedun <97944708+Foyedun@users.noreply.github.com>
2022-06-21 00:28:21 +0100

    Add files via upload

---
+++ mixarray.js
@@ -0,0 +1,18 @@
+function sumMix(arr){
+var total = 0;
+for (let i = 0; i<arr.length; i++)
+{
+    if (typeof arr[i]==="string")
+
+        total = total +Number([i]);
+        
+        {
+            elseif
+            {
+            total = total + arr[i];
+        }
+    }
+        return total
+    }
+}
+module.exports = sumMix
